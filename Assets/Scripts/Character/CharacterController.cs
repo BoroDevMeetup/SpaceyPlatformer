3899847b8d2cf7d0ac22bf67bbb44bed7fddf138
Matthew Caldwell <caldwellysr@gmail.com>
2020-04-09 21:51:58 -0500

    Add character that can fly between grabbable surfaces

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -1,13 +1,44 @@
 ﻿using UnityEngine;
 
 namespace Character { 
+	[RequireComponent(typeof(Rigidbody2D))]
 	public class CharacterController : MonoBehaviour {
-		void Start() {
-			
+		[SerializeField]
+		private Transform crosshairs = null;
+
+		[Header("Speed")]
+		[SerializeField]
+		[Range(0f, 100f)]
+		private float jumpPower = 5f;
+
+		private Rigidbody2D body;
+		private bool grounded = false;
+
+		private void Awake() {
+			body = GetComponent<Rigidbody2D>();
 		}
 
-		void Update() {
-			
+		private void Update() {
+			if (Input.GetButtonDown("Jump")) {
+				Jump();
+			}
+		}
+
+		void Jump() {
+			if (grounded) {
+				grounded = false;
+				Vector3 jumpVector = Vector3.Normalize(crosshairs.position - transform.position) * jumpPower;
+				body.AddForce(jumpVector, ForceMode2D.Impulse);
+			}
+		}
+
+		private void OnCollisionEnter2D(Collision2D collision) {
+			if (!grounded && collision.gameObject.CompareTag("Grabbable")) {
+				Debug.Log("Grab!");
+				grounded = true;
+				body.velocity = Vector3.zero;
+				body.angularVelocity = 0f;
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/Character/Crosshairs.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Character { 
+	public class Crosshairs : MonoBehaviour {
+		private void Awake() {
+			Cursor.visible = false;
+		}
+		private void Update() {
+            var mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
+		}
+	}
+}
